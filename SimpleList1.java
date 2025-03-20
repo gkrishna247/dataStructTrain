558640316639f389a86b94e23379571a432fad6f
gkrishna247 <krishnamoorthitech2224@gmail.com>
2025-03-20 10:12:25 +0530

    Add initial C and Java programs, .gitignore, and update note.txt

---
+++ SimpleList1.java
@@ -0,0 +1,46 @@
+public class SimpleList1 {
+
+    private Node head;
+
+    private static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // Adds a new element to the end of the list
+    public void add(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    // Prints all the elements in the list
+    public void printList() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        SimpleList1 list = new SimpleList1();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.printList(); // Output: 1 2 3
+    }
+}
