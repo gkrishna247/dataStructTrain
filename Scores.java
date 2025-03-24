d0ddcffea5ba58ff8d1d0a0c239c1e6354b55750
gkrishna247 <krishnamoorthitech2224@gmail.com>
2025-03-24 14:16:50 +0530

    Add Scores.java: implement basic structure with main method and string manipulation

---
+++ Scores.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class Scores{
+    public static void main(String [] args){
+        String st="Hi";
+        System.out.println(st.charAt(0));
+        if("1"=="11"){
+            System.err.println("Yes");
+        }
+    }
+}
