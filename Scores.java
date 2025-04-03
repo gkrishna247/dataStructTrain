e5399ee235c7aa390fd4790a9020c8391792e8dd
gkrishna247 <krishnamoorthitech2224@gmail.com>
2025-04-03 09:10:29 +0530

    Refactor Scores.java: remove unused import and fix string comparison using equals method

--- Scores.java
+++ Scores.java
@@ -1,10 +1,10 @@
-import java.util.Scanner;
+// Removed unused import
 
 class Scores{
     public static void main(String [] args){
         String st="Hi";
         System.out.println(st.charAt(0));
-        if("1"=="11"){
+        if("1".equals("11")){
             System.err.println("Yes");
         }
     }
