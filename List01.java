6eb79751d8c28dfa40afb1b8c9cfbbbe37346190
gkrishna247 <krishnamoorthitech2224@gmail.com>
2025-04-03 09:12:17 +0530

    Refactor List01.java: modify insert method to accept Scanner as a parameter and close it after use

--- List01.java
+++ List01.java
@@ -7,10 +7,9 @@ class Node1{
 
 class List01{
     
-    public static void insert(Node1 head){
+    public static void insert(Node1 head, Scanner sc){
         Node1 obj=new Node1();
         Node1 temp = head;
-        Scanner sc=new Scanner(System.in);
         obj.val=sc.nextInt();
         while(temp.next != null){
             temp=temp.next;
@@ -32,13 +31,13 @@ class List01{
     public static void main(String[] args){
         Node1 nl1=new Node1();
         //Node1 nl2=new Node1();
-        Scanner sc1 = new Scanner(System.in);
-        int s1=sc1.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int s1=sc.nextInt();
         for(int i=0;i<s1;i++){
-            insert(nl1);
+            insert(nl1,sc);
         }
         display(nl1);
-        sc1.close();
+        sc.close();
     }
 }
 
