408f208af0f445f9ecfa99e008864312ccd1444f
gkrishna247 <krishnamoorthitech2224@gmail.com>
2025-04-03 09:10:35 +0530

    Refactor SimpleList2.java: modify insert method to accept Scanner as a parameter and close it after use

--- SimpleList2.java
+++ SimpleList2.java
@@ -8,10 +8,9 @@ class Node
 
 class SimpleList2
 {
-    public static void insert(Node head)
+    public static void insert(Node head,Scanner sc)
     {
         Node ob=new Node();
-        Scanner sc = new Scanner(System.in);
         ob.roll = sc.nextInt();
         ob.next = head.next;
         head.next = ob;
@@ -29,11 +28,13 @@ class SimpleList2
     public static void main(String[] args)
     {
         Node head = new Node();
+        Scanner sc = new Scanner(System.in);
         head.next = null;
-        insert(head);
-        insert(head);
-        insert(head);
+        insert(head, sc);
+        insert(head, sc);
+        insert(head, sc);
         display(head);
+        sc.close();
 
     }
 } 
