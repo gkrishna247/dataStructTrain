a3043e785070250e4a1a9b504df5bbf69d540610
gkrishna247 <krishnamoorthitech2224@gmail.com>
2025-04-04 14:36:31 +0530

    Refactor Rtpattern class: modify output to print numbers instead of asterisks

--- Rtpattern.java
+++ Rtpattern.java
@@ -3,9 +3,11 @@ public class Rtpattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n=sc.nextInt();
+        int t=1;
         for(int r=0;r<n;r++){
             for(int c=0;c<=r;c++){
-                System.out.print("*");
+                System.out.print(t);
+                t++;
             }
             System.out.println();
         }
