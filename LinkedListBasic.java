4a7ae043c1d5f88f6eab0a74f2c56ac44fb6ddc6
gkrishna247 <krishnamoorthitech2224@gmail.com>
2025-04-16 11:09:09 +0530

    Program for performin basic operation on linked list is added

---
+++ LinkedListBasic.java
@@ -0,0 +1,101 @@
+import java.util.Scanner;
+
+class Node{
+    int val;
+    Node next;
+}
+
+public class LinkedListBasic {
+    public static void display(Node head){
+        Node temp=head;
+        while (temp.next!=null){
+            temp=temp.next;
+            System.out.print(temp.val+" ");
+        }
+        System.out.println();
+    }
+    public static void insertAtEnd(Node head,Scanner sc){
+        Node temp=head;
+        Node obj=new Node();
+        obj.val=sc.nextInt();
+        while(temp.next!=null){
+            temp=temp.next;
+        }
+        temp.next=obj;
+        obj.next=null;
+    }
+    public static void insertAtPos(Node head,int pos,int n){
+        Node temp=head;
+        int k=0;
+        while(temp.next!=null && k!=pos-1 && pos>0){
+            temp=temp.next;
+            k++;
+        }
+        if(k == pos-1){
+            Node obj=new Node();
+            obj.val=n;
+            obj.next=temp.next;
+            temp.next=obj;
+            System.out.print("The list after insertion is: ");
+            display(head);
+        }else{
+            System.out.println("Invalid Position: "+pos);
+        }
+    }
+    public static void deleteUserInput(Node head,Scanner sc){
+        Node temp=head;
+        System.out.print("Enter the value to delete from list: ");
+        int n=sc.nextInt();
+        while(temp.next!=null){
+            if(temp.next.val==n){
+                temp.next=temp.next.next;
+            }else{
+                temp=temp.next;
+            }
+        }
+    }
+    public static void deleteAtPos(Node head,int pos){
+        Node temp=head;
+        int k=0;
+        while(temp.next!=null && k!=pos-1){
+            temp=temp.next;
+        }
+        if(temp.next!=null && k==pos-1){
+            temp.next=temp.next.next;
+        }else{
+            System.out.println("Invalid Position: "+pos);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        Node head=new Node();
+        head.next=null;
+        System.out.print("Enter number of elements to be inserted in list:");
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            insertAtEnd(head, sc);
+        }
+        System.out.print("The intial list: ");
+        display(head);
+        for(int i=0;i<3;i++){
+            System.out.print("Enter position to insert: ");
+            int p=sc.nextInt();
+            System.out.print("Enter value to Insert: ");
+            int v=sc.nextInt();
+            insertAtPos(head, p, v);
+        }
+        for(int i=0;i<3;i++){
+            deleteUserInput(head, sc);
+            System.out.print("The array after deletion (user input): ");
+            display(head);
+        }
+        for(int i=0;i<3;i++){
+            System.out.print("Enter position to delete: ");
+            int p=sc.nextInt();
+            deleteAtPos(head, p);
+            System.out.print("The array after deletion: ");
+            display(head);
+        }
+        sc.close();
+    }
+}
