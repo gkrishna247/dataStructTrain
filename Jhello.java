6749b69dcd32c88bfb3b0cab651d03b0101d65cb
gkrishna247 <krishnamoorthitech2224@gmail.com>
2025-04-22 09:52:26 +0530

    Update Jhello class to print the result of 99999 divided by 1000 instead of uppercase letters

--- Jhello.java
+++ Jhello.java
@@ -1,7 +1,7 @@
 public class Jhello {
     public static void main(String[] args) {
         for(int i=0;i<26;i++){
-            System.out.println( (char)(i+'A') );
+            System.out.println( 99999/1000 );
         }
         }
 }
