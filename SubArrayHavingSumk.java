b31e0aa614374f178b0d0953b15f1829523dc8e2
gkrishna247 <krishnamoorthitech2224@gmail.com>
2025-04-29 13:32:11 +0530

    Add printSubSumIsK method to find and print subarrays with a given sum

--- SubArrayHavingSumk.java
+++ SubArrayHavingSumk.java
@@ -22,18 +22,36 @@ public class SubArrayHavingSumk{
             }
         }
     }
+    public static void printSubSumIsK(int arr[], int n, int k, int s, int e,int sum,ArrayList<Integer> sa) {
+        if (s == n) {
+            return;
+        }
+        if (e == n) {
+            sa.clear();
+            printSubSumIsK(arr, n, k, s+1, s+1, 0, sa);
+            return;
+        }
+        sa.add(arr[e]);
+        sum+=arr[e];
+        if(sum==k){
+            System.out.println(sa);
+        }
+        printSubSumIsK(arr, n, k, s, e+1, sum, sa);
+    }
+    
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter valur of n: ");
         int n=sc.nextInt();
         int a[]=new int[n];
-        System.out.println("Enter array Elements: ");
+        System.out.print("Enter array Elements: ");
         for(int i=0;i<n;i++){
             a[i]=sc.nextInt();
         }
-        System.out.print("Enter the value of k: ");
+        System.out.print("Enter the valur of'k': ");
         int k=sc.nextInt();
-        printSubHaveSumk(a, n, k, 0, 0);
+        ArrayList <Integer> sub=new ArrayList<>();
+        printSubSumIsK(a, n, k, 0, 0, 0, sub);
 
         sc.close();
     }
